4d71a9485420d888b2a8b6088d7d660e96c496fc
Julia Banerjee <juliabanerjee27@gmail.com>
2025-01-17 13:06:40 +0000

    worked on reset button

--- main.js
+++ main.js
@@ -10,27 +10,35 @@ const displayDiv = document.querySelector("#displayingtasks");
 
 const tasks = document.querySelector(".task")
 
+const resetButton = document.querySelector("#resetbutton")
 
 
 // adding event listener when enter button is pressed
 addButton.addEventListener("click", (event) =>{
 
-  //array of task to be displayed later
-  const taskArr = [];
-    //task is saved
+  // array of task to be displayed later
+  let taskArr = [];
+    // task is saved
     const taskInput = boxToAddTasks.value
     
     taskArr.push(taskInput);
   
     for(let i = 0; i<taskArr.length; i++){
-     displayDiv.innerHTML = taskArr[i]
-     
+     displayDiv.innerHTML += taskArr[i]
     }
-      
       
 
     console.log(taskArr)
     return taskArr
   
 });
+// unfinished reset button
+resetButton.addEventListener("click", (event) =>{
+
+taskArr = [];
+displayDiv.innerHTML = taskArr
+
+return taskArr
+
+})
 
